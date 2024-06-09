eb5701b32df6d59c55193f618094c331547b92b9
Ayush <ayush2ojha22ayush@gmail.com>
2024-06-09 19:30:16 +0530

    project init

---
+++ constants/prompts.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * Propt for extracting JD from
+ * 
+ * here is Html code for inkedin job page, extract out informations like copany name, job desciption, skills require and other possible and relevent info reated to job and provide me answer in bullet points or sections for each info extracted 
+ * 
+ * jobs-box__html-content 
+ * 
+ */
